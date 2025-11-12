2669356b6c21b22ba4b7226a57dc11de30bab1e5
Sanchita Sarker <141989731+404sanchita@users.noreply.github.com>
2025-11-12 20:19:34 +0600

    Update guide.js

--- models/guide.js
+++ models/guide.js
@@ -3,12 +3,13 @@ import mongoose from "mongoose";
 const guideSchema = new mongoose.Schema({
   name: { type: String, required: true },
   language: [String],
-  hourlyRate: Number,
-  experience: Number,
+  hourlyRate: String,
+  experience: String,
   location: String,
   specialties: [String],
-  availableDates: [Date],
+  unavailableDates: [Date],
 });
 
 export default mongoose.model("Guide", guideSchema);
+
 
