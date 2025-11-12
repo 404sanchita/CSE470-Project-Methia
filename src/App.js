73e1191be436af1078218b06e8b19d2a7d057ceb
AtikaTabassum <115262496+AtikaTabassum@users.noreply.github.com>
2025-11-12 20:00:45 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+// Pages
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
@@ -13,12 +15,31 @@ import Guides from "./pages/guide";
 import Booking from "./pages/booking";
 import Quiz from "./pages/quiz";
 
+// Extra components
+import HomePage from "./components/HomePage";
+import Profile from "./components/Profile";
+
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <AuthProvider>
         <Navbar />
+
+        {/* Simple extra nav bar */}
+        <nav
+          style={{
+            display: "flex",
+            gap: "1rem",
+            padding: "1rem",
+            background: "#eee",
+          }}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+        </nav>
+
         <Routes>
+          {/* Existing routes */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
@@ -35,9 +56,12 @@ export default function App() {
           <Route path="/guide" element={<Guides />} />
           <Route path="/booking" element={<Booking />} />
           <Route path="/quiz" element={<Quiz />} />
+
+          {/* New routes */}
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </AuthProvider>
-    </BrowserRouter>
+    </Router>
   );
 }
-
