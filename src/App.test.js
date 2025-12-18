4fb309396602668db8d934b809fb6ee6ee83e18e
AtikaTabassum <115262496+AtikaTabassum@users.noreply.github.com>
2025-12-18 23:21:51 +0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import ProtectedRoute from "../components/ProtectedRoute";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import HomePage from "../pages/HomePage/home";
+import Destination from "../pages/Destination/destination";
+import Guide from "../pages/Guide/guide";
+import Profile from "../pages/Profile/profile";
+import Login from "../pages/Login/login";
+import Signup from "../pages/Login/signup";
+
+import { AuthProvider } from "../context/AuthContext";
+
+function App() {
+  return (
+    <AuthProvider>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/destination/:id" element={<Destination />} />
+        <Route path="/guide" element={<Guide />} />
+
+        {/* 🔒 Protected Profile Route */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
+}
+
+export default App;
